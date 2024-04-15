ca3755f479263443c450a299f0b7274dd9d5bae5
mzaini30 <muhzaini30@gmail.com>
2024-04-15 18:45:14 +0800

    feat: daisy + legacy build

--- assets/vite.config.js
+++ assets/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import pages from "vite-plugin-pages-svelte";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), pages()],
+  plugins: [
+    svelte(),
+    pages(),
+    legacy({
+      renderModernChunks: false,
+    }),
+  ],
 });
